91d1675a31665cd5e3f69d8b2d1a2ca4adfd9a48
Umhar Hussain <130864554+Umhar97@users.noreply.github.com>
2023-09-15 19:10:55 +0100

    added submitSearchForm function that prevents defaulting

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 
-let searchInput = $("#search-input")
-let searchForm = $("#search-form")
+let searchInput = $("#search-input");
+let searchForm = $("#search-form");
 
+function submitSearchForm(event){
+
+    event.preventdefault();
+    let search = searchInput.val().trim()
+
+}
+
+searchForm.on("submit", submitSearchForm);
