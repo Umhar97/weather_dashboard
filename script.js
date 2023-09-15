3b98825d4d514d9a8db7482b194bb095fc4a9bdf
Umhar Hussain <130864554+Umhar97@users.noreply.github.com>
2023-09-15 19:24:52 +0100

    added weather api url and api key

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+const weatherAPIURL = "https://api.openweathermap.org"
+const weatherAPIkey = "db429ddea4a4a6848d93ba927020212b"
 
 
 let searchInput = $("#search-input");
 let searchForm = $("#search-form");
 
+function fetchCoord(){
+    let queryURL = `${weatherAPIURL}/geo1.0/direct?q=${search}&limit=5appid=${weatherAPIkey}`
+}
+
 function submitSearchForm(event){
 
     event.preventdefault();
     let search = searchInput.val().trim()
+
+    fetchCoord(search);
 
 }
 
